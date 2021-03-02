3836381331f9a52e8713b22c1ad84017e5e9c8ad
Stauroman <62332038+Stauroman@users.noreply.github.com>
2021-03-02 08:04:40 +0700

    мелкие правки

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'http://localhost:3000/api/v1/'
+});
+
+
+const App: React.FC = () => {
+    const [first, setfirst] = useState({});
+    const [second, setsecond] = useState({});
+    const [third, setthird] = useState({});
+    useEffect(() => {
+        getData("first", 0);
+        getData("second", 1);
+        getData("third", 2);
+    }, []);
+
+    const setarr = [setfirst, setsecond, setthird]
+
+    const getData = async (market: string, index: number) => {
+        await instance.get(`${market}`)
+            .then(response => {
+                setarr[index](response.data)
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+    console.log("first", first)
+    console.log('second',second)
+    console.log('third',third)
+
+
+    return (
+        <div>
+
+        </div>
+    );
 }
 
 export default App;
